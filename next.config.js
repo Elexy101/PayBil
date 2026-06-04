edbe08304577cc925d2cab7dd49627ac4248fc92
Emmanuel Ekpe <Emmanuellinus013@gmail.com>
2026-06-04 03:59:27 +0100

    Remove turbopack configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {
-    root: '/home/emmanuel/ai_agent_pay/sources',
-  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
